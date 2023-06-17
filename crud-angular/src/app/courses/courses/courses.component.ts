8a52ba9f79dedada3e2107b85e67f8030efbadf9
Ricardo Alves <dev.rical21@gmail.com>
2023-06-16 22:45:40 -0300

    Compartilhar módulos, lazy-load, icones e erros.

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,20 +1,39 @@
+import { Observable, catchError, of } from 'rxjs';
 import { CoursesService } from '../services/courses.service';
 import { Course } from './../model/course';
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss'],
 })
+//
 export class CoursesComponent implements OnInit {
   //
-  courses: Course[] = [];
+  courses$: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
-  constructor(private coursesService: CoursesService) {}
-
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+  ) {
+    this.courses$ = this.coursesService.list().pipe(
+      catchError((error) => {
+        this.onError('Erro ao carregar cursos');
+        return of([]);
+      })
+    );
+  }
   ngOnInit(): void {
-    this.courses = this.coursesService.list();
+    throw new Error('Method not implemented.');
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg,
+    });
   }
 }
